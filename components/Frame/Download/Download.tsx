a374fdafcddd1af95671ed30b4d41996d0311886
Brent Ahrens <cidida@gmail.com>
2021-04-30 22:35:53 -0700

    create download frame

--- components/Frame/Download/Download.tsx
+++ components/Frame/Download/Download.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react'
+
 const Download: React.FC = () => {
+    const [ isPhoneDrop, setIsPhoneDrop ] = useState<boolean>(false)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsPhoneDrop(true)
+        }, 2000)
+
+        return () => clearInterval(interval)
+    }, [])
+
     return (
-        <div>
-            <h1>Download</h1>
+        <div className="download">
+            <div className="download__left-tree">
+                <img src="/images/main/left-tree.png" alt="Young Planet | left tree" />
+            </div>
+            <div className="download__right-tree">
+                <img src="/images/main/right-tree.png" alt="Young Planet | right tree" />
+            </div>
+            <div className={`download__phone ${isPhoneDrop ? "phone-down" : ""}`}>
+                <img src="/images/frame/download/phone.png" alt="Young Planet | floating phone" />
+            </div>
+            <div className="download__hand">
+                <img src="/images/frame/download/hand.png" alt="Young Planet | floating hand" />
+            </div>
         </div>
     )
 }
--- components/Frame/Listen/DogDays/DogDays.tsx
+++ components/Frame/Listen/DogDays/DogDays.tsx
@@ -2,11 +2,20 @@ import { useEffect, useState } from "react"
 
 const DogDays: React.FC = () => {
     const [ isDogOut, setIsDogOut ] = useState<boolean>(false)
+    const [ isPaperOut, setPaperOut ] = useState<boolean>(false)
 
     useEffect(() => {
         const interval = setInterval(() => {
             setIsDogOut(true)
-        }, 4000)
+        }, 2500)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setPaperOut(true)
+        }, 3500)
 
         return () => clearInterval(interval)
     }, [])
@@ -19,13 +28,13 @@ const DogDays: React.FC = () => {
             <div className="dog__right-tree">
                 <img src="/images/main/right-tree.png" alt="Young Planet | right tree" />
             </div>
-            <div className={`dog__dog ${isDogOut ? "dog-out": ""}`}>
+            <div className={`dog__dog ${isDogOut ? "dog-out": ""} ${isPaperOut ? "dog-back" : ""}`}>
                 <img src="/images/frame/listen/dog/dog.png" alt="Young Planet | dog" />
             </div>
             <div className="dog__door">
                 <img src="/images/frame/listen/dog/door.png" alt="Young Planet | door" />
             </div>
-            <div className="dog__paper">
+            <div className={`dog__paper ${isPaperOut ? "paper-out" : ""}`}>
                 <img src="/images/frame/listen/dog/paper.png" alt="Young Planet | newspaper" />
             </div>
         </div>
