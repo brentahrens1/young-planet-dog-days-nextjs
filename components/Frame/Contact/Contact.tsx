5ed7472adfe8c087d0a9560ed4f3c66da047d651
Brent Ahrens <cidida@gmail.com>
2021-05-01 20:07:06 -0700

    set up contact frame

--- components/Frame/Contact/Contact.tsx
+++ components/Frame/Contact/Contact.tsx
@@ -1,7 +1,30 @@
 const Contact: React.FC = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="contact">
+            <div className="contact__left-tree">
+                <img src="/images/main/left-tree.png" alt="Young Planet | left tree" />
+            </div>
+            <div className="contact__right-tree">
+                <img src="/images/main/right-tree.png" alt="Young Planet | right tree" />
+            </div>
+            <div className="contact__water-top">
+                <img src="/images/main/land-and-water/water-thinner.png" alt="Young Planet | water top" />
+            </div>
+            <div className="contact__water-bottom">
+                <img src="/images/main/land-and-water/water.png" alt="Young Planet | water bottom" />
+            </div>
+            <div className="contact__mountains">
+                <img src="/images/main/land-and-water/mountains.png" alt="Young Planet | water bottom" />
+            </div>
+            <div className="contact__crab">
+                <img src="/images/frame/contact/crab.png" alt="Young Planet | crab" />
+            </div>
+            <div className="contact__bottle">
+                <img src="/images/frame/contact/bottle.png" alt="Young Planet | bottle" />
+            </div>
+            <div className="contact__plane">
+                <img src="/images/frame/contact/plane-right.png" alt="Young Planet | plane" />
+            </div>
         </div>
     )
 }
