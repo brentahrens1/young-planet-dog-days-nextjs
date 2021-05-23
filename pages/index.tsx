570f060b2abbf51632a5200e23a0156f62666263
Brent Ahrens <cidida@gmail.com>
2021-05-23 14:09:46 -0700

    adds meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,11 @@ import Home from '../components/Home/Home'
 export default function App() {
   return (
     <main className="site-container">
+      <Head>
+        <title>Young Planet | Dog Days</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Dog Days EP now available on all streaming platforms." />
+      </Head>
       <Home />
     </main>
   )
