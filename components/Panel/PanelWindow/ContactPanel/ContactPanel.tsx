770b261837fb521e4bc5be8a6f1a28b7952c6ddf
Brent Ahrens <cidida@gmail.com>
2021-05-23 14:34:01 -0700

    add social links

--- components/Panel/PanelWindow/ContactPanel/ContactPanel.tsx
+++ components/Panel/PanelWindow/ContactPanel/ContactPanel.tsx
@@ -3,10 +3,10 @@ const ContactPanel = () => {
         <div className="contact-panel">
             <div className="contact-panel__inner">
                 <div className="icon">
-                    <img src="/images/panel/contact/instagram.png" alt="Young Planet | Instagram" />
+                    <a href="https://www.instagram.com/young_planet/" target="_blank"><img src="/images/panel/contact/instagram.png" alt="Young Planet | Instagram" /></a>
                 </div>
                 <div className="icon">
-                    <img src="/images/panel/contact/twitter.png" alt="Young Planet | Twitter" />
+                    <a href="https://www.twitter.com/spinyoungplanet" target="_blank"><img src="/images/panel/contact/twitter.png" alt="Young Planet | Twitter" /></a>
                 </div>
                 <div className="icon">
                     <img src="/images/panel/contact/mail.png" alt="Young Planet | Email" />
